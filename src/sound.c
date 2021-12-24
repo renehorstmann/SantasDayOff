079747790bc3f71d3e56b083564ee23fe4b0fbe6
renehorstmann <r.horsty@gmail.com>
2021-12-24 09:10:30 +0100

    gpush

--- src/sound.c
+++ src/sound.c
@@ -22,7 +22,7 @@ static void init(Sound *self) {
     
     // sdl mixer default hz was 22050
     
-    if (Mix_OpenAudio(22050/2, MIX_DEFAULT_FORMAT, 1, 64) == -1) {
+    if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 128) == -1) {
         log_warn("sound not working");
         return;
     }
